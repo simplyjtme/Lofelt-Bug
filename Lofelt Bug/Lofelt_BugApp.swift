db414a2c518cd8f3dcc5a7aa031e29e490a33e8e
JT <jt@simplyjt.me>
2021-07-27 18:03:23 -0500

    Initial Commit

---
+++ Lofelt Bug/Lofelt_BugApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lofelt_BugApp.swift
+//  Lofelt Bug
+//
+//  Created by Joseph Thryselius on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Lofelt_BugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
